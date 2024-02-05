8a3f90efbb2a667586feeb534a746d93b3b7a369
rick <LinuxSuRen@users.noreply.github.com>
2024-02-05 08:09:28 +0000

    provide the initial status of the helm docker

---
+++ helm-build-and-push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+server=$1
+username=$2
+password=$3
+repo=$4
+sourceDir=$5
+filename=$6
+
+if [[ "$server" == "" || "$username" == "" || "$password" == "" || "$repo" == "" || "$sourceDir" == "" || "$filename" == "" ]]; then
+    echo "Usage: $0 <server> <username> <password> <repo> <sourceDir> <filename>"
+    exit 1
+fi
+
+
+helm registry login ${server} --insecure --username ${username} --password ${password}
+
+helm package ${sourceDir}
+
+helm push ${filename} oci://${server}/${repo} --plain-http
